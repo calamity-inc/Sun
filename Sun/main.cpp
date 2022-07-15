a01490d63835e8fcd173920801eba1228d0d8607
Sainan <63328889+Sainan@users.noreply.github.com>
2022-07-15 02:10:54 +0200

    Support multiple .cpp file parameters

--- Sun/main.cpp
+++ Sun/main.cpp
@@ -4,22 +4,66 @@
 #include <soup/Compiler.hpp>
 #include <soup/main.hpp>
 
+[[nodiscard]] static std::string remove_extension(const std::string& file)
+{
+	auto ext = std::filesystem::path(file).extension().string();
+	auto name = file.substr(0, file.length() - ext.length());
+	return name;
+}
+
 int entry(std::vector<std::string>&& args, bool console)
 {
 	if (args.size() < 2)
 	{
-		std::cout << "Syntax: sun <path>" << std::endl;
+		std::cout << "Syntax: sun <.cpp file paths ...>" << std::endl;
 		return 1;
 	}
 
-	auto cpp = args.at(1);
-	auto ext = std::filesystem::path(cpp).extension().string();
-	auto name = cpp.substr(0, cpp.length() - ext.length());
+	std::vector<std::string> objects{};
 
-	auto exe = name;
+	if (!std::filesystem::is_directory("int"))
+	{
+		std::filesystem::create_directory("int");
+	}
+
+	std::cout << "Compiling..." << std::endl;
+	args.erase(args.begin());
+	for (const auto& cpp : args)
+	{
+		auto name = remove_extension(cpp);
+
+		std::cout << name << std::endl;
+
+		std::string o = "int/";
+		o.append(name);
+		o.append(".o");
+
+		std::cout << soup::Compiler::makeObject(cpp, o);
+
+		objects.emplace_back(o);
+	}
+
+	std::cout << "Linking..." << std::endl;
+
+	std::string exe{};
+	if (args.size() == 1)
+	{
+		exe = remove_extension(args.at(0));
+	}
+	else
+	{
+		exe = std::filesystem::current_path().filename().string();
+	}
+#if SOUP_WINDOWS
 	exe.append(".exe");
+#endif
 
-	std::cout << soup::Compiler::makeExecutable(cpp, exe);
+	auto linkout = soup::Compiler::makeExecutable(objects, exe);
+	if (!linkout.empty())
+	{
+		std::cout << linkout;
+		return 1;
+	}
 
 	return 0;
 }
---
+++ examples/exe-cli/message.cpp
@@ -0,0 +1,1 @@
+const char* message = "Hello, world!";
